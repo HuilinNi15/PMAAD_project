d704cdc158347cab437428bda1e9d32571c8869f
Núria Bosch Martínez <nuria.bosch.martinez@estudiantat.upc.edu>
2023-03-20 22:01:48 +0100

    He intentat fer el test MCAR, però falla, també he fet correlacions

--- inici.R
+++ inici.R
@@ -24,6 +24,38 @@ df <- mutate_all(df, funs(replace(., .=='UNKNOWN', NA)))
 sum(is.na(df))
 
 df <- df[,!colnames(bb) %in% c("Crash ID", "Adjusted Average Daily Traffic Amount", "Average Daily Traffic Year", "Case ID", "City", "County", "Crash Possible Injury Count", "Crash Unknown Injury Count", "Population Group", "Person Type", "Street Number", "Street Name", "Roadway relation")]
+#df <- df[,!colnames(bb) %in% c("Crash ID", "Adjusted Average Daily Traffic Amount", "Average Daily Traffic Year", "Case ID", "City", "County", "Crash Possible Injury Count", "Crash Unknown Injury Count", "Population Group", "Person Type", "Street Number", "Street Name", "Roadway relation", "Crash Total Injury Count")]
+
 
 sum(is.na(df))
 
+
+df$`Right of Way Usual Width` <- as.numeric(df$`Right of Way Usual Width`)
+df$`Speed Limit` <- as.numeric(df$`Speed Limit`)
+df$`Number of Lanes` <- as.numeric(df$`Number of Lanes`)
+df$`Outside Shoulder Width on Divided Highway` <- as.numeric(df$`Outside Shoulder Width on Divided Highway`)
+df$Latitude <- as.numeric(df$Latitude)
+df$Longitude <- as.numeric(df$Longitude)
+df$`Crash Incapacitating Injury Count` <- as.numeric(df$`Crash Incapacitating Injury Count`)
+df$`Average Daily Traffic Amount` <- as.numeric(df$`Average Daily Traffic Amount`)
+
+
+
+library(naniar)
+mcar_test(df)
+
+#If the p-value > 0.05, missing data is missing completely at random.
+
+library(missForest)
+MCARNNTest(df)
+
+
+
+num_cols <- unlist(lapply(df, is.numeric))
+data_num <- df[ , num_cols]
+#numèriques
+cormat <- round(cor(data_num, use = "pairwise.complete.obs"), 2)
+#mixed data
+library(GGally)
+# S'ha de dir el número de variables que vols, com a màxim són 15
+fggpairs(df, columns = 1:4, title = "Pairwise Correlations")
